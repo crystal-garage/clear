32710869e1d906eac7c8caa6db923e9533b5745e
Anton Maminov <anton.linux@gmail.com>
2022-11-11 17:14:20 +0200

    extract Clear::Interval and Clear::TimeInDay

--- src/clear/extensions/time/interval.cr
+++ src/clear/extensions/time/interval.cr
@@ -1,70 +1,14 @@
-# Represents the "interval" object of PostgreSQL
-struct Clear::Interval
-  getter microseconds : Int64 = 0
-  getter days : Int32 = 0
-  getter months : Int32 = 0
-
-  def initialize(span : Time::Span)
-    @microseconds = span.total_nanoseconds.to_i64 // 1_000
+struct Time
+  def +(i : Clear::Interval)
+    self + i.months.months + i.days.days + i.microseconds.microseconds
   end
 
-  def initialize(span : Time::MonthSpan)
-    @months = span.value.to_i32
-  end
-
-  def initialize(
-    years : Number = 0,
-    months : Number = 0,
-    weeks : Number = 0,
-    days : Number = 0,
-    hours : Number = 0,
-    minutes : Number = 0,
-    seconds : Number = 0,
-    milliseconds : Number = 0,
-    microseconds : Number = 0
-  )
-    @months = (12 * years + months).to_i32
-    @days = days.to_i32
-    @microseconds = (
-      microseconds.to_i64 +
-      milliseconds * 1_000_i64 +
-      seconds * 1_000_000_i64 +
-      minutes * 60_000_000_i64 +
-      hours * 3_600_000_000_i64
-    )
-  end
-
-  def initialize(io : IO)
-    @microseconds = io.read_bytes(Int64, IO::ByteFormat::BigEndian)
-    @days = io.read_bytes(Int32, IO::ByteFormat::BigEndian)
-    @months = io.read_bytes(Int32, IO::ByteFormat::BigEndian)
-  end
-
-  def to_sql
-    o = [] of String
-
-    (o << @months.to_s << "months") if @months != 0
-    (o << @days.to_s << "days") if @days != 0
-    (o << @microseconds.to_s << "microseconds") if @microseconds != 0
-
-    Clear::SQL.unsafe({
-      "INTERVAL",
-      Clear::Expression[o.join(" ")],
-    }.join(" "))
-  end
-
-  def +(i : self)
-    self.new(months: self.months + i.months, day: self.days + i.days, microseconds: self.microseconds + i.microseconds)
-  end
-
-  def self.decode(x : Slice(UInt8))
-    io = IO::Memory.new(x, writeable: false)
-
-    self.new(io)
+  def -(i : Clear::Interval)
+    self - i.months.months - i.days.days - i.microseconds.microseconds
   end
 end
 
-class Clear::Model::Converter::IntervalConverter
+class Clear::Interval::Converter
   def self.to_column(x) : Clear::Interval?
     case x
     when PG::Interval
@@ -85,14 +29,4 @@ class Clear::Model::Converter::IntervalConverter
   end
 end
 
-struct Time
-  def +(i : Clear::Interval)
-    self + i.months.months + i.days.days + i.microseconds.microseconds
-  end
-
-  def -(i : Clear::Interval)
-    self - i.months.months - i.days.days - i.microseconds.microseconds
-  end
-end
-
-Clear::Model::Converter.add_converter("Clear::Interval", Clear::Model::Converter::IntervalConverter)
+Clear::Model::Converter.add_converter("Clear::Interval", Clear::Interval::Converter)
